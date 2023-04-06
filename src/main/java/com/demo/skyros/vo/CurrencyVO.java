5f495cc2c3b3e8de07cafcc32e29d6b0e52e807b
ahmed.baz <ahmed.baz@fawry.com>
2023-04-06 17:19:15 +0200

    add lombok

--- src/main/java/com/demo/skyros/vo/CurrencyVO.java
+++ src/main/java/com/demo/skyros/vo/CurrencyVO.java
@@ -1,44 +1,20 @@
 package com.demo.skyros.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyVO {
 
     private String from;
     private String to;
     private BigDecimal quantity;
     private String tag;
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public BigDecimal getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(BigDecimal quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
 }
